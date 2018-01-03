2a83b3bf49acd47299e733a739d0bb632ef5b38a
Evan <evanjordandweck@gmail.com>
2018-01-03 16:24:17 -0500

    updated db schema to include gradepoint avgs for a course.  Also changed primary key for Course to a combo of coursename and course department

--- create_schema.sql
+++ create_schema.sql
@@ -1,29 +1,45 @@
-DROP TABLE Professor;
+DROP TABLE Instructor;
 CREATE TABLE Professor (
-  professorId INTEGER NOT NULL,
+  instructorId INTEGER NOT NULL,
   firstName VARCHAR(30) NOT NULL,
   lastName VARCHAR(30) NOT NULL,
-  PRIMARY KEY (professorId)
+  PRIMARY KEY (instructorId)
 );
 
 DROP TABLE Course;
 CREATE TABLE Course (
-  courseId INTEGER NOT NULL,
   courseName VARCHAR(30) NOT NULL,
+  courseDepartment VARCHAR(30) NOT NULL,
   courseNumber VARCHAR(30) NOT NULL,
+  averageRating DOUBLE, 
+  PRIMARY KEY (courseDepartment, courseNumber),
+  FOREIGN KEY (courseDepartment, courseNumber) REFERENCES GradePoint (courseId)
+  ON DELETE CASCADE
+);
+
+DROP TABLE GradePoint;
+CREATE TABLE GradePoint (
+  courseId VARCHAR(30) NOT NULL,
+  fallSem15 DOUBLE,
+  springSem15 DOUBLE,
+  fallSem16 DOUBLE,
+  springSem16 DOUBLE,
+  fallSem16 DOUBLE,
+  springSem16 DOUBLE,
   PRIMARY KEY (courseId)
 );
 
 DROP TABLE CourseReview;
 CREATE TABLE CourseReview (
   reviewId INTEGER NOT NULL,
-  courseId INTEGER NOT NULL,
+  courseId VARCHAR(30) NOT NULL,
   professorId INTEGER NOT NULL,
-  review VARCHAR(30) NOT NULL,
+  reviewerId INTEGER NOT NULL,
+  review VARCHAR(100) NOT NULL,
   rating INTEGER NOT NULL,
   reviewDate DATE NOT NULL,
   PRIMARY KEY (reviewId),
-  FOREIGN KEY (courseId) REFERENCES Course(courseId),
+  FOREIGN KEY (courseId) REFERENCES Course(courseDepartment, courseNumber),
   FOREIGN KEY (professorId) REFERENCES Professor(professorId)
   ON DELETE CASCADE
 );
